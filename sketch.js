889d894c5ab2efd810fd4c1eef02753dac6b1bf4
Madhavan <madhavanraja99@gmail.com>
2019-07-16 18:15:08 +0530

    Added Regression Visualization

--- sketch.js
+++ sketch.js
@@ -12,9 +12,30 @@ function draw()
 	for (i = 0; i < points.length; i++)
 	{
 		let cur = points[i];
-
 		ellipse(cur[0], cur[1], 10, 10);
 	}
+
+	let n = points.length;
+	let sumxy = 0;
+	let sumx = 0;
+	let sumxq = 0;
+	let sumy = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		let cur = points[i];
+		sumxy += cur[0] * cur[1];
+		sumx += cur[0];
+		sumxq += cur[0] * cur[0];
+		sumy += cur[1];
+	}
+
+	let m = (n * sumxy - sumx * sumy) / (n * sumxq - sumx * sumx);
+	let c = (sumy - m * sumx) / n;
+
+	stroke(255);
+	strokeWeight(2);
+	line(0, c, 600, m * 600 + c);
 }
 
 function mousePressed()
